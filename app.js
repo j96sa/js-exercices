7e79453dae091b36201d1f07540c31e2ad060375
Josue Paulo Antuan <josue96sa@gmail.com>
2022-02-08 20:12:05 +0100

    merged

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /*******************************************************************************************  SUMAR max-min */
 // 1 -- crear una funcion que se le pasen como parametros dos arreglos y que devuelva la suma del menor y el mayor numero de cada array
 
-const arr1 = [20,56,80,100],
-arr2 = [1,1,1,2,10];
+/* const arr1 = [20,56,80,100],
+arr2 = [1,1,1,2,10]; */
 
 //SOLUCION EN ENTREVISTA
-function maxmin(number){
+/* function maxmin(number){
     const result = number.reduce((acc,number)=>{
         if(acc.max < number) acc.max = number;
         if(acc.min > number) acc.min = number;
@@ -13,7 +13,7 @@ function maxmin(number){
     },{min:Infinity,max:0});
     console.log(result.min + result.max);
 }   
-maxmin(arr1);
+maxmin(arr1); */
 
 //OPCION-1
 /* const sumar = (max,min)=>{  
@@ -37,10 +37,61 @@ sumar(max,min); */
 
 
 
-/*   */
 
 
 
 
+/*(Free Code Camp) SUMAR NUMEROS EN UN ARRAY  */
+// 2 -- Le pasaremos una matriz de dos números. Devuelve la suma de esos dos números más la suma de todos los números entre ellos. El número más bajo no siempre vendrá primero.
+
+//sumAll([1, 4]) debe devolver 10.
+//sumAll([4, 1]) debe devolver 10.
+//sumAll([5, 10]) debe devolver 45.
+//sumAll([10, 5]) debe devolver 45.
+
+/* const sumAll = (arr)=>{    
+    const min = Math.min(...arr),
+    max = Math.max(...arr),
+    
+    newNumbers = [];
+
+    for(let i = min+1; i<max; i++){
+        newNumbers.push(i);
+    };
+
+    const newArr = newNumbers.concat(arr)
+    return console.log(newArr.reduce((acc ,num) => acc + num));
+};
+sumAll([1,4]);
+sumAll([4,1]);
+sumAll([5,10]);
+sumAll([10,5]); */
+
+
+
+
+
+
+
+
+
+/*(Free Code Camp) COMPARAR MATRICES */
+//Compare dos matrices y devuelva una nueva matriz con cualquier elemento que solo se encuentre en una de las dos matrices dadas, pero no en ambas. En otras palabras, devuelva la diferencia simétrica de las dos matrices.
+//Nota: puede devolver la matriz con sus elementos en cualquier orden.
+
+const arr1 = [1,2,3],
+arr2 = [1,2,3,88];
+
+
+const compareArr = (a,b)=>{
+    const newArr = [];
+
+    a.forEach(element => {
+        b.map(e=>e===element ?false :newArr.push(element))   
+    });
+    
+};
+
+compareArr(arr1,arr2)
 
 
